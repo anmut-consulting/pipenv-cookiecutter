c38e2ac3c0a9e651d4b072945bfc3aca08c3f826
Sean Soutar <sean.soutar@anmut.co.uk>
2021-04-09 15:49:40 +0100

    changed pipx installs to curly brackets grouping without a continue line

--- basics.sh
+++ basics.sh
@@ -68,7 +68,7 @@
     source ~/.zshrc
     echo "\n-> installing software from homebrew casks:"
     echo "this is a minimal list to get you started"
-    (
+    {
         brew install --cask docker
         brew install --cask google-chrome
         brew install --cask dashlane
@@ -78,18 +78,17 @@
         brew install --cask pycharm-ce
         brew install --cask drawio
         pipx ensurepath && source ~/.zshrc
-    ) \
-    && echo "\n-> installation from homebrew casks successfull <-\n"
+    } && echo "\n-> installation from homebrew casks successfull <-\n"
     echo "\n-> installing from pipx:"
-    echo "   pipenv pipenv-pipes pipenv-setup pre-commit" \
-    && (
-        pipx install pipenv \
-        && pipx install pipenv-pipes \
-        && pipx install pipenv-setup \
-        && pipx install pre-commit ) \
-    && echo "finished installing python tools"
-    source ~/.zshrc \
-    && echo "\n-> pipx installations complete <-" \
+    echo "   pipenv pipenv-pipes pipenv-setup pre-commit"
+    {
+        pipx install pipenv
+        pipx install pipenv-pipes
+        pipx install pipenv-setup
+        pipx install pre-commit
+    } && echo "finished installing python tools"
+    source ~/.zshrc
+    echo "\n-> pipx installations complete <-" \
     && echo "\n-> configuring git to run pre-commit hooks, when found, automatically:" \
     && git config --global init.templateDir ~/.git-template \
     && pre-commit init-templatedir ~/.git-template -t pre-commit -t pre-push \
