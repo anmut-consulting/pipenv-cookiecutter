61497fc7289b75cfc1e834d0556d0d7490a8fb99
Sean Soutar <sean.soutar@anmut.co.uk>
2021-04-09 16:11:14 +0100

    Update basics.sh (#14)
    * Updated brew install --cask syntax. Removed some cask installs. Added exporting of SYSTEM_VERSION_COMPAT to the .zshrc file to avoid mysterious Big Sur errors
    
    * Github Action to test basics.sh install on a PR
    
    - Checkout PR Branch
    - Run basics.sh install on a BigSure 11.0 macOS Runner
    
    * Removed some whitespaces in script
    
    * removed blank line at the end of the script
    
    * echo statement to verify end of line issue
    
    * removed authy and heroku installs
    
    * put source .zshrc on a separate line
    
    * changed pipx installs to curly brackets grouping without a continue line
    
    * Removed window prompt to close terminals. Users need to close everything manually. Posed difficulties in Github Action as event times out

--- basics.sh
+++ basics.sh
@@ -14,7 +14,6 @@
     xcode-select --install \
     && echo "\n-> xcode cli installed <-\n" \
     || echo " "
-
     echo "\n-> installing homebrew: \n"
     {
         if ! [[ $(brew --version) ]];
@@ -27,7 +26,6 @@
         && {
           brew install zsh
           brew install python3
-          brew install awscli
           brew install cmake
           brew install git
           brew install htop
@@ -46,11 +44,10 @@
           brew install pipx
           brew install pyenv
           brew install watch
-          brew tap heroku/brew && brew install heroku
+#          brew tap heroku/brew && brew install heroku
         }
     } \
     && echo "\n-> packages installed from brew! <-\n"
-
     touch ~/.zshrc
     if ! grep -Fxq 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc
     then
@@ -64,59 +61,41 @@
     then
         echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
     fi
-    source ~/.zshrc \
-
+    if ! grep -Fxq 'export SYSTEM_VERSION_COMPAT=1' ~/.zshrc
+    then
+        echo 'export SYSTEM_VERSION_COMPAT=1' >> ~/.zshrc
+    fi
+    source ~/.zshrc
     echo "\n-> installing software from homebrew casks:"
-    echo "   google-chrome lastpass mysqlworkbench slack sublime-text the-unarchiver whatsapp"
+    echo "this is a minimal list to get you started"
     {
-        brew cask install authy
-        brew cask install docker
-        brew cask install google-chrome
-        brew cask install lastpass
-        brew cask install mysqlworkbench
-        brew cask install pg-commander
-        brew cask install sublime-text
-        brew cask install the-unarchiver
+        brew install --cask docker
+        brew install --cask google-chrome
+        brew install --cask dashlane
+        brew install --cask dbeaver-community
+        brew install --cask sublime-text
+        brew install --cask the-unarchiver
+        brew install --cask pycharm-ce
+        brew install --cask drawio
         pipx ensurepath && source ~/.zshrc
-        # Uncomment block if you want to install the microsoft stuff
-        echo "\n-> installing microsoft stuff from homebrew casks:"
-        echo "   microsoft-edge microsoft-office microsoft-teams"
-        brew cask install microsoft-edge
-        brew cask install microsoft-teams
-        brew cask install drawio
-    } \
-    && echo "\n-> installation from homebrew casks successfull <-\n"
-
-    echo "\n-> installing from pipx: "
+    } && echo "\n-> installation from homebrew casks successfull <-\n"
+    echo "\n-> installing from pipx:"
     echo "   pipenv pipenv-pipes pipenv-setup pre-commit"
-    (
-        pipx install pipenv \
-        && pipx install pipenv-pipes \
-        && pipx install pipenv-setup \
-        && pipx install pre-commit
-    ) \
-    && source ~/.zshrc \
-    && echo "\n-> pipx installations complete <-" \
+    {
+        pipx install pipenv
+        pipx install pipenv-pipes
+        pipx install pipenv-setup
+        pipx install pre-commit
+    } && echo "finished installing python tools"
+    source ~/.zshrc
+    echo "\n-> pipx installations complete <-" \
     && echo "\n-> configuring git to run pre-commit hooks, when found, automatically:" \
     && git config --global init.templateDir ~/.git-template \
     && pre-commit init-templatedir ~/.git-template -t pre-commit -t pre-push \
-    && echo "\n-> pre-commit configured <-"
-
-    echo "\nIf you don't yet know magnet for OSX, you're wasting time resizing windows!"
-    echo "Here's the link: https://apps.apple.com/gb/app/magnet/id441258766?mt=12"
-    read -p "Do you want to open that link now? [y/n]" -t 5 REPLY
-    case $REPLY in
-        [yY])
-            open "https://apps.apple.com/gb/app/magnet/id441258766?mt=12"
-        ;;
-        *)
-        ;;
-    esac
-) \
+    && echo "\n-> pre-commit configured <-" ) \
 && (
     echo "\n"
     echo "================== INSTALLATION COMPLETE! =================="
     echo "CLOSE ALL TERMINAL WINDOWS AND REOPEN TO ENSURE CORRECT PATH"
     echo "================== INSTALLATION COMPLETE! =================="
-    echo "\n"
-) && osascript -e 'tell application "Terminal" to close (every window)' && exit 0 || exit 1
+    echo "\n" ) && echo 'tell application "Terminal" to close (every window)' && exit 0 || exit 1
