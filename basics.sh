224b974608f6c5151f32b90580ec9b43362df69b
Chris Sutcliffe <106994397+csut-dev@users.noreply.github.com>
2023-02-21 16:36:32 +0000

    Update basics.sh with working environment variables (#35)
    * remove dashlane from script as the cask no longer exists
    
    * replace echo with printf to ensure correct output, add checks for compiler flags to ensure not added twice
    
    * add comments to script where appropriate
    
    * remove echo header comment, add spacing where logical

--- basics.sh
+++ basics.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 (
-    echo "\n-> installing xcode: \n"
+    printf "\n-> installing xcode: \n\n"
+    # Check if xcode configured and if not set it up on your machine
     if ! [[ -x $(xcode-select -p) ]];
     then 
         xcode-select -s /Applications/Xcode.app/Contents/Developer \
@@ -8,21 +9,25 @@
             && xcode-select -s /Applications/Xcode.app/Contents/Developer) \
         && xcodebuild -license
     fi
-    echo "\n-> xcode installed <-\n"
+    printf "\n-> xcode installed <-\n\n"
 
-    echo "\n-> installing xcode cli: \n"
+    printf "\n-> installing xcode cli: \n\n"
     xcode-select --install \
-    && echo "\n-> xcode cli installed <-\n" \
-    || echo " "
-    echo "\n-> installing homebrew: \n"
+    && printf "\n-> xcode cli installed <-\n\n" \
+    || printf "\n"
+
+    printf "\n-> installing homebrew: \n\n"
     {
+        # Check if brew is installed, if not, download the install script using
+        # curl, run it and return to this script after the installation is
+        # complete.
         if ! [[ $(brew --version) ]];
         then
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         fi \
-        && echo "\n-> homebrew installed <-\n" \
-        && echo "\n-> installing from brew:" \
-        && echo "     zsh python3 cmake git htop openssl readline sqlite3 tree unar xz zlib pipx pyenv\n" \
+        && printf "\n-> homebrew installed <-\n\n" \
+        && printf "\n-> installing from brew:\n" \
+        && printf "     zsh python3 cmake git htop openssl readline sqlite3 tree unar xz zlib pipx pyenv\n\n" \
         && {
           brew install zsh
           brew install python3
@@ -47,51 +52,88 @@
 #          brew tap heroku/brew && brew install heroku
         }
     } \
-    && echo "\n-> packages installed from brew! <-\n"
+    && printf "\n-> packages installed from brew! <-\n\n"
+
+    # Create a zshrc file if it does not exist already. If it does, touch will
+    # simply update the timestamp of the file.
+    #
+    # The zshrc file stands for zsh run command and is a file used to customise
+    # your shell environment. It contains a list of commands that are run every
+    # time a new terminal window is opened.
+    # Here we are adding environment variables to the file for pyenv, one of the
+    # python virtual environment systems.
+
+    # For each line to be inserted into the file, we first check if the line
+    # already exists in the file using grep, and only if it's not present is it
+    # added in. This prevents these env variables being added twice if this
+    # script is ever re-run.
     touch ~/.zshrc
+
     if ! grep -Fxq 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc
     then
-        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+        printf 'export PYENV_ROOT="$HOME/.pyenv"\n' >> ~/.zshrc
     fi
     if ! grep -Fxq 'export PATH="$PYENV_ROOT:$PATH"' ~/.zshrc
     then
-        echo 'export PATH="$PYENV_ROOT:$PATH"' >> ~/.zshrc
+        printf 'export PATH="$PYENV_ROOT:$PATH"\n' >> ~/.zshrc
     fi
+
+    # Insert the pyenv init command into zshrc if it does not already exist. The
+    # inserted code first checks if pyenv is installed and if present will
+    # configure pyenv as a shell function and enable shims and shell
+    # autocompletion.
     if ! grep -Fxq 'if command -v pyenv 1>/dev/null 2>&1; then' ~/.zshrc
     then
-        echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+        printf 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi\n' >> ~/.zshrc
     fi
+
+    # Re-initialise the shell environment, loading the above shell variables
+    # and running the pyenv init command
     source ~/.zshrc
-    echo "\n-> installing software from homebrew casks:"
-    echo "this is a minimal list to get you started"
+
+    printf "\n-> installing software from homebrew casks:\n"
+    printf "this is a minimal list to get you started\n"
     {
         brew install --cask docker
-        brew install --cask dashlane
         brew install --cask dbeaver-community
         brew install --cask sublime-text
         brew install --cask the-unarchiver
         brew install --cask drawio
         pipx ensurepath && source ~/.zshrc
-    } && echo "\n-> installation from homebrew casks successfull <-\n"
-    echo "Adding compiler flags for bzip2"
-    echo "export LDFLAGS=-L/usr/local/opt/bzip2/lib" >> ~/.zshrc
-    echo "export CPPFLAGS=-I/usr/local/opt/bzip2/include" >> ~/.zshrc
-    echo "\n-> installing from pipx:"
-    echo "   pipenv pipenv-pipes pre-commit"
+    } && printf "\n-> installation from homebrew casks successfully <-\n\n"
+
+    # Insert compiler flags to the zshrc file only if they're not already
+    # present
+    printf "Adding compiler flags for bzip2\n"
+    if ! grep -Fxq 'export LDFLAGS=-L/usr/local/opt/bzip2/lib' ~/.zshrc
+    then
+        printf "export LDFLAGS=-L/usr/local/opt/bzip2/lib\n" >> ~/.zshrc
+    fi
+    if ! grep -Fxq 'export CPPFLAGS=-I/usr/local/opt/bzip2/include' ~/.zshrc
+        then
+    printf "export CPPFLAGS=-I/usr/local/opt/bzip2/include\n" >> ~/.zshrc
+    fi
+
+    printf "\n-> installing from pipx:\n"
+    printf "   pipenv pipenv-pipes pre-commit\n"
     {
         pipx install pipenv
         pipx install pipenv-pipes
         pipx install pre-commit
-    } && echo "finished installing python tools"
+    } && printf "finished installing python tools\n"
+
+    # Re-initialise the shell environment again, reflecting the changes made
+    # above.
     source ~/.zshrc
-    echo "\n-> pipx installations complete <-" \
-    && echo "\n-> configuring git to run pre-commit hooks, when found, automatically:" \
+
+    printf "\n-> pipx installations complete <-\n" \
+    && printf "\n-> configuring git to run pre-commit hooks, when found, automatically:\n" \
     && git config --global init.templateDir ~/.git-template \
     && pre-commit init-templatedir ~/.git-template -t pre-commit -t pre-push \
-    && echo "\n-> pre-commit configured <-" ) \
+    && printf "\n-> pre-commit configured <-\n" ) \
 && (
-    echo "\n"
-    echo "================== INSTALLATION COMPLETE! =================="
-    echo "CLOSE ALL TERMINAL WINDOWS AND REOPEN TO ENSURE CORRECT PATH"
-    echo "================== INSTALLATION COMPLETE! =================="
-    echo "\n" ) && echo 'tell application "Terminal" to close (every window)' && exit 0 || exit 1
+    printf "\n\n"
+    printf "================== INSTALLATION COMPLETE! ==================\n"
+    printf "CLOSE ALL TERMINAL WINDOWS AND REOPEN TO ENSURE CORRECT PATH\n"
+    printf "================== INSTALLATION COMPLETE! ==================\n"
+    printf "\n\n" ) && printf 'tell application "Terminal" to close (every window)\n' && exit 0 || exit 1
